1ec96c86098775c23a73946a05459ed080269ccd
pgm-chardelv1 <chardelv1@student.arteveldehs.be>
2021-06-04 09:59:34 +0200

    Fix lint errors

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { AuthProvider, FirebaseProvider } from './contexts/firebase';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import * as Routes from './routes';
 
 import styles from './App.module.scss';
-import { HomePage, MoviePage, MoviesPage, ShowPage, ShowsPage, SignInPage } from './pages';
+import {
+  HomePage,
+  MoviePage,
+  MoviesPage,
+  ShowPage,
+  ShowsPage,
+  SignInPage,
+} from './pages';
 import { FirestoreProvider } from './contexts/firebase/firestore.context';
 
 function App() {
@@ -13,15 +20,27 @@ function App() {
       <FirebaseProvider>
         <AuthProvider>
           <FirestoreProvider>
-            <Router basename={'movie-application-pgm-chardelv1'}>
+            <Router basename="movie-application-pgm-chardelv1">
               <Switch>
-                  <Route exact path={Routes.LANDING} component={ HomePage }/>
-                  <Route from={Routes.HOME} to={Routes.LANDING}/>
-                  <Route exact path={Routes.MOVIE_DETAILS} component={ MoviePage }/>
-                  <Route exact path={Routes.MOVIES} component={ MoviesPage }/>
-                  <Route exact path={Routes.TVSHOW_DETAILS} component={ ShowPage }/>
-                  <Route exact path={Routes.TVSHOWS} component={ ShowsPage }/>
-                  <Route exact path={Routes.AUTH_SIGN_IN} component={ SignInPage }/>
+                <Route exact path={Routes.LANDING} component={HomePage} />
+                <Route from={Routes.HOME} to={Routes.LANDING} />
+                <Route
+                  exact
+                  path={Routes.MOVIE_DETAILS}
+                  component={MoviePage}
+                />
+                <Route exact path={Routes.MOVIES} component={MoviesPage} />
+                <Route
+                  exact
+                  path={Routes.TVSHOW_DETAILS}
+                  component={ShowPage}
+                />
+                <Route exact path={Routes.TVSHOWS} component={ShowsPage} />
+                <Route
+                  exact
+                  path={Routes.AUTH_SIGN_IN}
+                  component={SignInPage}
+                />
               </Switch>
             </Router>
           </FirestoreProvider>
--- client/src/app/hooks/useFetch.js
+++ client/src/app/hooks/useFetch.js
@@ -1,38 +1,39 @@
 import { useEffect, useState } from 'react';
 /**
  * Use Fetch
- * @param {string} endpoint 
- * @param {string} queryParams 
+ * @param {string} endpoint
+ * @param {string} queryParams
  * @returns {array} [{array} data, {boolean} isLoading, {error/null} error]
  */
 const useFetch = (endpoint, queryParams = '') => {
   // If there are query params defined append them to the API URL
+  let params;
   if (queryParams !== '') {
-    queryParams = `&${queryParams}`;
+    params = `&${queryParams}`;
   }
-  const [ data, setData ] = useState([]);
-  const [ error, setError ] = useState();
-  const [ isLoading, setIsLoading ] = useState(true);
-  const apiUrl = `https://api.themoviedb.org/3/${endpoint}?api_key=${process.env.REACT_APP_TMDB_API_KEY}${queryParams}`
+  const [data, setData] = useState([]);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const apiUrl = `https://api.themoviedb.org/3/${endpoint}?api_key=${process.env.REACT_APP_TMDB_API_KEY}${params}`;
 
   const getData = async () => {
     try {
       const response = await fetch(apiUrl);
-      const data = await response.json();
-      setData(data);
+      const jsonData = await response.json();
+      setData(jsonData);
     } catch (err) {
-      setError(err)
+      setError(err);
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     getData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return [data, isLoading, error];
-}
+};
 
 export default useFetch;
